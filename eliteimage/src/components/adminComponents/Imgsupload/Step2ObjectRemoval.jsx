c55ab55ecb97c122fd26f6b434e0b2528bbc0d29
Numaan Ali <alinumaan35@gmail.com>
2026-03-06 14:59:39 +0500

    ok

--- eliteimage/src/components/adminComponents/Imgsupload/Step2ObjectRemoval.jsx
+++ eliteimage/src/components/adminComponents/Imgsupload/Step2ObjectRemoval.jsx
@@ -359,7 +359,7 @@ const Step2ObjectRemoval = ({ formData, setFormData, next, back }) => {
               width={800}
               height={600}
               sizes="(max-width: 768px) 100vw, 800px"
-              className="w-full select-none"
+              className="w-full h-auto select-none"
               draggable={false}
               priority
             />
