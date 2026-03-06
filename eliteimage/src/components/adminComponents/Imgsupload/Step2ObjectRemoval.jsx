953ef8c4046b0ff2658318e2b945d136ed994fe7
Numaan Ali <alinumaan35@gmail.com>
2026-03-06 14:44:53 +0500

    ok

--- eliteimage/src/components/adminComponents/Imgsupload/Step2ObjectRemoval.jsx
+++ eliteimage/src/components/adminComponents/Imgsupload/Step2ObjectRemoval.jsx
@@ -340,7 +340,7 @@ const Step2ObjectRemoval = ({ formData, setFormData, next, back }) => {
           </div>
 
           <div
-            className="relative w-full bg-white rounded-[40px] overflow-hidden cursor-crosshair"
+            className="relative w-full bg-white rounded-[40px] overflow-hidden cursor-crosshair h-32 sm:h-36 lg:h-40"
             onMouseDown={isProcessing ? undefined : handleMouseDown}
             onMouseMove={handleMouseMove}
             onMouseUp={handleMouseUp}
@@ -359,7 +359,7 @@ const Step2ObjectRemoval = ({ formData, setFormData, next, back }) => {
               width={800}
               height={600}
               sizes="(max-width: 768px) 100vw, 800px"
-              className="w-full h-auto select-none"
+              className="w-full h-32 sm:h-36 lg:h-40 object-contain select-none"
               draggable={false}
               priority
             />
