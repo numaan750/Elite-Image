d735d1199ba62bf131b801b96379237e29c864e7
Numaan Ali <alinumaan35@gmail.com>
2026-03-11 13:17:59 +0500

    ok

--- eliteimage/src/components/adminComponents/Imgsupload/Step2.jsx
+++ eliteimage/src/components/adminComponents/Imgsupload/Step2.jsx
@@ -49,10 +49,10 @@ const Step2 = ({ formData, setFormData, next, back, featureType }) => {
 
   const handleContinue = () => {
     setFormData((prev) => ({
-  ...prev,
-  selectedFeatures: selected,
-  selectedFeature: featureType === "enhancement" ? selected : selected[0] || "",
-}));
+      ...prev,
+      selectedFeatures: selected,
+      selectedFeature: selected[0] || "",
+    }));
     next();
   };
 
@@ -73,16 +73,8 @@ const Step2 = ({ formData, setFormData, next, back, featureType }) => {
           <div
             key={item.name}
             onClick={() => {
-  if (featureType === "enhancement") {
-    setSelected((prev) =>
-      prev.includes(item.name)
-        ? prev.filter((i) => i !== item.name)
-        : [...prev, item.name]
-    );
-  } else {
-    setSelected([item.name]);
-  }
-}}
+              setSelected([item.name]);
+            }}
             className={`rounded-xl cursor-pointer border-2 overflow-hidden transition-all hover:shadow-lg ${
               selected.includes(item.name)
                 ? "border-[#034F75] shadow-md"
